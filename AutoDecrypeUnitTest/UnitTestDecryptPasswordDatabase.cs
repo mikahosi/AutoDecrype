f3eaf8b81cd406e6d2ae12c5635f3f7b04a46905
Kunihiro Narita <mikahosi@abox9.so-net.ne.jp>
2016-11-22 17:12:14 +0900

    パスワードデータベースを作成する処理

---
+++ AutoDecrypeUnitTest/UnitTestDecryptPasswordDatabase.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AutoDecryptCore;
+
+namespace AutoDecrypeUnitTest
+{
+    /// <summary>
+    /// UnitTestCreateOrReplaceBlankDatabase の概要の説明
+    /// </summary>
+    [TestClass]
+    public class UnitTestDecryptPasswordDatabase
+    {
+        public UnitTestDecryptPasswordDatabase()
+        {
+            //
+            // TODO: コンストラクター ロジックをここに追加します
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        //
+        // テストを作成する際には、次の追加属性を使用できます:
+        //
+        // クラス内で最初のテストを実行する前に、ClassInitialize を使用してコードを実行してください
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // クラス内のテストをすべて実行したら、ClassCleanup を使用してコードを実行してください
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 各テストを実行する前に、TestInitialize を使用してコードを実行してください
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 各テストを実行した後に、TestCleanup を使用してコードを実行してください
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void CreateOrReplaceBlankDatabase()
+        {
+            DecryptPasswordDatabase db = new DecryptPasswordDatabase();
+
+            db.RemoveDatabase();
+
+            Assert.AreEqual(false, db.ExistDatabase());
+
+            db.CreateOrReplaceBlankDatabase();
+
+            Assert.AreEqual(true, db.ExistDatabase());
+
+            db.CreateOrReplaceBlankDatabase();
+
+            Assert.AreEqual(true, db.ExistDatabase());
+        }
+
+        [TestMethod]
+        public void AddOrGetDecryptPassword()
+        {
+            string testAddress = "mailaddress";
+            string testPassword = "DecryptPasswordDatabase";
+            DateTime dateDate = DateTime.Now;
+
+            DecryptPasswordDatabase db = new DecryptPasswordDatabase();
+            db.CreateOrReplaceBlankDatabase();
+            db.AddDecryptPassword(testAddress, testPassword, dateDate);
+
+            var recs = db.GetDecryptPassword();
+            Assert.AreEqual(1, recs.Count);
+            Assert.AreEqual(testAddress, recs[0].fromMailAddress);
+            Assert.AreEqual(testPassword, recs[0].decryptPassword);
+            Assert.AreEqual(dateDate.ToLongDateString(), recs[0].mailSendDataTime.ToLongDateString());
+            Assert.AreEqual(dateDate.ToLongTimeString(), recs[0].mailSendDataTime.ToLongTimeString());
+        }
+    }
+}
---
+++ AutoDecryptCore/DecryptPasswordDatabase.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AutoDecryptCore
+{
+    using System.Data.SQLite;
+    using System.Data.SQLite.Linq;
+
+    public class DecryptPasswordArticle
+    {
+        public int id;
+        public string fromMailAddress { get; set; }
+        public string decryptPassword { get; set; }
+        public DateTime mailSendDataTime { get; set; }
+    }
+
+    public class DecryptPasswordDatabase
+    {
+        string db_file = "password.db3";
+
+        public DecryptPasswordDatabase()
+        {
+        }
+
+        public void AddDecryptPassword(string fromMailAddress, string decryptPassword, DateTime mailSendDataTime)
+        {
+            using (var conn = CreateConnection())
+            {
+                conn.Open();
+                using (SQLiteCommand command = conn.CreateCommand())
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine("insert into DecryptPassword");
+                    sql.AppendLine("    ( fromMailAddress, decryptPassword, mailSendDataTime )");
+                    sql.AppendLine("    values (?, ?, ?)");
+                    command.CommandText = sql.ToString();
+                    command.Parameters.Add(new SQLiteParameter("fromMailAddress", fromMailAddress));
+                    command.Parameters.Add(new SQLiteParameter("decryptPassword", decryptPassword));
+                    command.Parameters.Add(new SQLiteParameter("mailSendDataTime", mailSendDataTime.ToString("yyyy-MM-dd HH:mm:ss")));
+                    command.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
+
+        public List<DecryptPasswordArticle> GetDecryptPassword()
+        {
+            List<DecryptPasswordArticle> passwordList = new List<DecryptPasswordArticle>();
+            using (var conn = CreateConnection())
+            {
+                conn.Open();
+                using (SQLiteCommand command = conn.CreateCommand())
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine("select id, fromMailAddress, decryptPassword, mailSendDataTime from DecryptPassword");
+                    command.CommandText = sql.ToString();
+                    var rdr = command.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        DecryptPasswordArticle rec = new DecryptPasswordArticle();
+                        rec.id = rdr.GetInt32(0);
+                        rec.fromMailAddress = rdr.GetString(1);
+                        rec.decryptPassword = rdr.GetString(2);
+                        rec.mailSendDataTime = DateTime.Parse(rdr.GetString(3));
+                        passwordList.Add(rec);
+                    }
+                }
+                conn.Close();
+            }
+            return passwordList;
+        }
+
+
+        public List<string> GetPasswordList()
+        {
+            List<string> passwordList = new List<string>();
+            using (var conn = CreateConnection())
+            {
+                conn.Open();
+                using (SQLiteCommand command = conn.CreateCommand())
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine("select decryptPassword from DecryptPassword");
+                    command.CommandText = sql.ToString();
+                    var rdr = command.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        passwordList.Add(rdr.GetString(0));
+                    }
+                }
+                conn.Close();
+            }
+            return passwordList;
+        }
+
+        public void CreateOrReplaceBlankDatabase()
+        {
+            RemoveDatabase();
+
+            using (var conn = CreateConnection())
+            {
+                conn.Open();
+                using (SQLiteCommand command = conn.CreateCommand())
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine("create table DecryptPassword (");
+                    sql.AppendLine("    id INTEGER PRIMARY KEY AUTOINCREMENT, ");
+                    sql.AppendLine("    fromMailAddress TEXT, ");
+                    sql.AppendLine("    decryptPassword TEXT, ");
+                    sql.AppendLine("    mailSendDataTime TEXT");
+                    sql.AppendLine(")");
+                    command.CommandText = sql.ToString();
+                    command.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
+
+        public bool ExistDatabase()
+        {
+            bool isSanity = false;
+            try
+            {
+                using (var conn = CreateConnection())
+                {
+                    conn.Open();
+                    using (SQLiteCommand command = conn.CreateCommand())
+                    {
+                        StringBuilder sql = new StringBuilder();
+                        sql.AppendLine("select * from DecryptPassword");
+                        command.CommandText = sql.ToString();
+                        command.ExecuteNonQuery();
+                        isSanity = true;
+                    }
+                    conn.Close();
+                }
+            }
+            catch (Exception exp)
+            {
+
+            }
+            return isSanity;
+        }
+
+        public void RemoveDatabase()
+        {
+            // 既存のファイルを削除する
+            if (File.Exists(db_file))
+            {
+                File.Delete(db_file);
+            }
+        }
+
+        private SQLiteConnection CreateConnection()
+        {
+            int retryCnt = 0;
+            SQLiteConnection conn = null;
+
+            while (true)
+            {
+                try
+                {
+                    conn = new SQLiteConnection("Data Source=" + db_file);
+                    break;
+                }
+                catch (Exception exp)
+                {
+                    retryCnt++;
+                    Thread.Sleep(1000);
+                }
+            }
+
+            return conn;
+        }
+    }
+}
