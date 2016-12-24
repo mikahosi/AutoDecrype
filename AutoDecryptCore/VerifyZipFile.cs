c43758f92f4d873e5d3b0c4c7fa41879c714a250
Kunihiro Narita <mikahosi@abox9.so-net.ne.jp>
2016-12-24 16:59:11 +0900

    パスワード試験のスケルトン追加

---
+++ AutoDecryptCore/VerifyZipFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDecryptCore
+{
+    class VerifyZipFile
+    {
+        private string zipFileName;
+        private List<string> passwordList;
+
+        public VerifyZipFile(string sourceZipFileName, string[]sourcePasswordList)
+        {
+            zipFileName = sourceZipFileName;
+            passwordList = new List<string>(sourcePasswordList);
+        }
+
+        /// <summary>
+        /// 対象となるZIPファイルが暗号化されているか判定する
+        /// </summary>
+        /// <returns></returns>
+        public bool isEncrypted()
+        {
+            return false;
+        }
+
+        public string searchTruePassword()
+        {
+            return "";
+        }
+    }
+}
